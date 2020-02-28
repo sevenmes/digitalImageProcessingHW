eb09dc8a41fffeaf65e9a9ad5775d1acca18e31a
sevenmes <shuziqi0517@icloud.com>
2020-02-28 14:51:27 +0800

    L1_2

---
+++ L1_2_gray/L1_2_gray/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  L1_2_gray
+//
+//  Created by 舒子琪 on 2020/2/28.
+//  Copyright © 2020 舒子琪. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using std::string;
+
+
+int main(int argc, const char * argv[]) {
+    
+    cv::Mat srcMat = imread("/Users/shuziqi/Desktop/test.jpg",0);
+    
+    imshow("src", srcMat);
+    
+    waitKey(0);
+    
+    return 0;
+}
