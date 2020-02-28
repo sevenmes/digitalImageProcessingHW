bed4d257b944088aa99436ade60dc972da28d76b
sevenmes <shuziqi0517@icloud.com>
2020-02-28 15:47:54 +0800

    L1_4

---
+++ L1_4_copy/L1_4_copy/main.cpp
@@ -0,0 +1,67 @@
+//
+//  main.cpp
+//  L1_4_copy
+//
+//  Created by 舒子琪 on 2020/2/28.
+//  Copyright © 2020 舒子琪. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using std::string;
+
+
+int main()
+{
+    
+    uchar threshold=100;
+    
+    cv::Mat srcMat = imread("/Users/shuziqi/Desktop/test.jpg");
+    
+    cv::Mat deepMat;
+    cv::Mat shallowMat;
+    
+    srcMat.copyTo(deepMat);
+    shallowMat = srcMat;
+    
+    int height = srcMat.rows;//行数，即图片的高
+    
+    int width = srcMat.cols;//每行元素的总元素量
+    
+    for (int j = 0;j < height; j++)
+    {
+        for (int i = 0;i < width;i++)
+        {
+            //开始处理每个像素
+            uchar average = (srcMat.at<Vec3b>(j, i)[0] +
+                             srcMat.at<Vec3b>(j, i)[1] +
+                             srcMat.at<Vec3b>(j, i)[2])/3;
+            
+            if (average > threshold)
+                
+                average = 255;
+            
+            else average = 0;
+            
+            srcMat.at<Vec3b>(j, i)[0] = average;
+            srcMat.at<Vec3b>(j, i)[1] = average;
+            srcMat.at<Vec3b>(j, i)[2] = average;
+            //结束像素处理
+        }//单行处理结束
+    }
+    
+    imshow("src", srcMat);
+    
+    waitKey(0);
+    
+    imshow("deepMat", deepMat);
+    
+    waitKey(0);
+    
+    imshow("shallowMat", shallowMat);
+    
+    waitKey(0);
+    
+    return 0;
+}
