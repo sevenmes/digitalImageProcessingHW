e7f219c0da1663de092b93e842808eda163ab656
sevenmes <shuziqi0517@icloud.com>
2020-02-28 14:44:52 +0800

    L1_!

---
+++ L1_1_calcPixelAverage/L1_1_calcPixelAverage/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  L1_1_calcPixelAverage
+//
+//  Created by 舒子琪 on 2020/2/28.
+//  Copyright © 2020 舒子琪. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using std::string;
+
+
+int main(int argc, const char * argv[]) {
+    
+    cv::Mat srcMat = imread("/Users/shuziqi/Desktop/test.jpg");
+    
+    int height = srcMat.rows;//行数，即图片的高
+    
+    int width = srcMat.cols;//每行元素的总元素量
+    
+    for (int j = 0;j < height; j++)
+    {
+        for (int i = 0;i < width;i++)
+        {
+            //开始处理每个像素
+            uchar average = (srcMat.at<Vec3b>(j, i)[0] +
+                             srcMat.at<Vec3b>(j, i)[1] +
+                             srcMat.at<Vec3b>(j, i)[2])/3;
+            
+            srcMat.at<Vec3b>(j, i)[0] = average;
+            srcMat.at<Vec3b>(j, i)[1] = average;
+            srcMat.at<Vec3b>(j, i)[2] = average;
+            //结束像素处理
+        }//单行处理结束
+    }
+    
+    imshow("src", srcMat);
+    
+    waitKey(0);
+    
+    return 0;
+}
