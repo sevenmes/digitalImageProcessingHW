be31184fd046d09425597fc1be7b0eb66e41fafd
sevenmes <shuziqi0517@icloud.com>
2020-03-06 18:05:20 +0800

    修改直线坐标

--- L2_3_paint/L2_3_paint/main.cpp
+++ L2_3_paint/L2_3_paint/main.cpp
@@ -19,6 +19,14 @@ int main()
     pt.x = 10;
     pt.y = 10;
     circle(dispMat,pt,5,CV_RGB(255,0,0),1,8,0);
+    
+    cv::Point pt1;
+    cv::Point pt2;
+    pt1.x = 0;
+    pt1.y = 0;
+    pt2.x = 80;
+    pt2.y = 80;
+    line(dispMat,pt1,pt2,CV_RGB(255, 0, 0),1,8,0);
     line(dispMat,pt1,pt2,CV_RGB(255,0,0),1,8,0);
     cv::Rect rect;
     rect.x = 10;
