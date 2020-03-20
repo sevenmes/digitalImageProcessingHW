f7384ebbf461fa42914b5c9cbd6646f9efdea366
sevenmes <shuziqi0517@icloud.com>
2020-03-20 14:54:13 +0800

    L4_2

---
+++ L4_2_connectedDomainMarker/L4_2_connectedDomainMarker/main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  L4_2_connectedDomainMarker
+//
+//  Created by 舒子琪 on 2020/3/20.
+//  Copyright © 2020 舒子琪. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using std::string;
+
+
+int main() {
+    cv::Mat binaryMat;
+    cv::Mat labelMat;
+    cv::Mat statsMat;
+    cv::Mat centrMat;
+
+    cv::Mat srcMat = imread("/Users/shuziqi/Desktop/coin.png",0);
+    int nComps;
+    threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+    
+    nComps = cv::connectedComponentsWithStats (binaryMat,
+                                                    labelMat,
+                                                       statsMat,
+                                                centrMat,
+                                                       8,
+                                                       CV_32S);
+
+    for( int i = 1; i < nComps; i++ ) {
+    
+        Rect bndbox;
+        bndbox.x = statsMat.at<int>(i,0);
+        bndbox.y = statsMat.at<int>(i,1);
+        bndbox.width = statsMat.at<int>(i,2);
+        bndbox.height = statsMat.at<int>(i,3);
+        rectangle(binaryMat, bndbox, CV_RGB(255,255,255),1,8,0);
+    }
+
+    imshow("结果图", binaryMat);
+    waitKey(0);
+}
+
