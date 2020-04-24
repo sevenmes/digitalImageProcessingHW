0270da6c7b5151650719cc3decc2265706fb770b
sevenmes <shuziqi0517@icloud.com>
2020-04-25 02:23:53 +0800

    L9

--- L9_1_HOG/L9_1_HOG/main.cpp
+++ L9_1_HOG/L9_1_HOG/main.cpp
@@ -13,72 +13,74 @@ using namespace cv;
 using namespace std;
 using std::string;
 
-float *hog(Mat srcMat)
+float *hog(Mat srcMat) {
+    int cellSize = 16;
+    int nX = srcMat.cols / cellSize;
+    int nY = srcMat.rows / cellSize;
+    
+    int cellnum = nX * nY;
+    int bins = cellnum * 8;
+    int binnum = 0;
+    
+    float* ref_hist = new float[bins];
+    memset(ref_hist, 0, sizeof(float) * bins);
+
+    //计算梯度及角度
+    Mat gx, gy;
+    Mat mag, angle;
+
+    Sobel(srcMat, gx, CV_32F, 1, 0, 1);
+    Sobel(srcMat, gy, CV_32F, 0, 1, 1);
+    
+    cartToPolar(gx, gy, mag, angle, true);
+
+    for (int i = 0; i < nX; i++){
+        for (int j = 0; j < nY; j++) {
+            
+            for (int a = j * cellSize; a < (j + 1)*cellSize; a++) {
+                for (int b = i * cellSize; b < (i + 1)*cellSize; b++) {
+                    int n = angle.at<float>(a, b) / 45;
+                    float m = mag.at<float>(a, b);
+                    ref_hist[n + binnum * 8] += m;
+                }
+                }
+            binnum++;
+            
+            }
+        }
+    return ref_hist;
+}
+
+float distance(Mat& srcMat, Mat& img)
 {
     int cellSize = 16;
     int nX = srcMat.cols / cellSize;
     int nY = srcMat.rows / cellSize;
-    
-    int cellnums = nX * nY;
-    int bins = cellnums * 8;
-    int binnumber = 0;
-    
-    float* ref_hist = new float[bins];
-    memset(ref_hist, 0, sizeof(float) * bins);
-    
-    for(int i = 0; i < nX; ++i)
-    {
-        for(int j = 0; j < nY; ++j)
-        {
-            //计算梯度及角度
-            Mat gx, gy;
-            Mat mag, angle;
-            
-            Sobel(srcMat, gx, CV_32F, 1, 0, 1);
-            Sobel(srcMat, gy, CV_32F, 0, 1, 1);
-            
-            cartToPolar(gx, gy, mag, angle, true);
-            
-            for (int a = i * cellSize; a < (i + 1)*cellSize; a++)
-            {
-                for (int b = j * cellSize; b < (j + 1)*cellSize; b++)
-                {
-                    int n = angle.at<float>(a, b) / 45;
-                    float m = mag.at<float>(a, b);
-                    ref_hist[n + binnumber * 8] += m;
-                }
-            }
-            binnumber++;
-        }
-    }
-    delete[] ref_hist;
-    return ref_hist;
-}
+    int cellnum = nX * nY;
+    int bins = cellnum * 8;
 
-int main() {
-    cv::Mat srcMat = cv::imread("/Users/shuziqi/Desktop/hogTemplate.jpg");
-    cv::Mat img1 = cv::imread("/Users/shuziqi/Desktop/img1");
-    cv::Mat img2 = cv::imread("/Users/shuziqi/Desktop/img2");
-
-    int cellSize = 16;
-    int nX = srcMat.cols / cellSize;
-    int nY = srcMat.rows / cellSize;
-    
-    int cellnums = nX * nY;
-    int bins = cellnums * 8;
-    
-    double n, m, dis1, dis2;
-    double d1 = 0, d2 = 0;
+    float a;
+    float d1 = 0;
+    float dis;
     
     for (int i = 0; i < bins; i++)
     {
-        n = (hog(srcMat)[i] - hog(img1)[i]);
-        m = (hog(srcMat)[i] - hog(img2)[i]);
-        d1 += n * n;
-        d2 += m * m;
+        a = hog(srcMat)[i] - hog(img)[i];
+        d1 += a * a;
     }
-    dis1 = sqrt(d1);
-    dis2 = sqrt(d2);
+    dis = sqrt(d1);
+
+    return dis;
+}
+
+int main() {
+    Mat srcMat = imread("/Users/shuziqi/Desktop/hogTemplate.jpg");
+    Mat img1 = imread("/Users/shuziqi/Desktop/img1.jpg");
+    Mat img2 = imread("/Users/shuziqi/Desktop/img2.jpg");
+    
+    float dis1, dis2;
+    dis1 = distance(srcMat, img1);
+    dis2 = distance(srcMat, img2);
     
     cout << dis1 << endl;
     cout << dis2 << endl;
