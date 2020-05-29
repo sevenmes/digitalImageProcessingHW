df74ca8549d36320e06128b899287fcef7a4ac11
sevenmes <shuziqi0517@icloud.com>
2020-05-29 17:46:25 +0800

    L13

---
+++ L13_1_location/L13_1_location/main.cpp
@@ -0,0 +1,108 @@
+//
+//  main.cpp
+//  L13_1_location
+//
+//  Created by 舒子琪 on 2020/5/29.
+//  Copyright © 2020 舒子琪. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <math.h>
+using namespace cv;
+using namespace std;
+using std::string;
+
+float *hog(Mat srcMat) {
+    int cellSize = 16;
+    int nX = srcMat.cols / cellSize;
+    int nY = srcMat.rows / cellSize;
+    
+    int cellnum = nX * nY;
+    int bins = cellnum * 8;
+    int binnum = 0;
+    
+    float* ref_hist = new float[bins];
+    memset(ref_hist, 0, sizeof(float) * bins);
+
+    //计算梯度及角度
+    Mat gx, gy;
+    Mat mag, angle;
+
+    Sobel(srcMat, gx, CV_32F, 1, 0, 1);
+    Sobel(srcMat, gy, CV_32F, 0, 1, 1);
+    
+    cartToPolar(gx, gy, mag, angle, true);
+
+    for (int i = 0; i < nX; i++){
+        for (int j = 0; j < nY; j++) {
+            
+            for (int a = j * cellSize; a < (j + 1)*cellSize; a++) {
+                for (int b = i * cellSize; b < (i + 1)*cellSize; b++) {
+                    int n = angle.at<float>(a, b) / 45;
+                    float m = mag.at<float>(a, b);
+                    ref_hist[n + binnum * 8] += m;
+                }
+                }
+            binnum++;
+            
+            }
+        }
+    return ref_hist;
+}
+
+float distance(Mat& srcMat, Mat& img)
+{
+    int cellSize = 16;
+    int nX = srcMat.cols / cellSize;
+    int nY = srcMat.rows / cellSize;
+    int cellnum = nX * nY;
+    int bins = cellnum * 8;
+
+    float a;
+    float d1 = 0;
+    float dis;
+    
+    for (int i = 0; i < bins; i++)
+    {
+        a = hog(srcMat)[i] - hog(img)[i];
+        d1 += a * a;
+    }
+    dis = sqrt(d1);
+
+    return dis;
+}
+
+int main() {
+    Mat srcMat = imread("/Users/shuziqi/Desktop/template.png");
+    Mat img = imread("/Users/shuziqi/Desktop/img.png");
+
+    float dis1, dis2;
+    int a, b;
+    //利用窗口对图像进行遍历
+    for (int i = 0; i < img.rows - srcMat.rows; i++)
+    {
+        for (int j = 0; j < img.cols - srcMat.cols; j++)
+        {
+            Rect rect1(j, i, srcMat.cols, srcMat.rows);
+            Mat ROI = img(rect1);
+            dis2 = distance(srcMat, ROI);
+            if(i==0 & j==0)
+            {
+                dis1 = dis2;
+            }
+            if (dis2 < dis1)
+            {
+                dis1 = dis2;
+                a = i;
+                b = j;
+            }
+        }
+    }
+    Rect rect2(b, a, srcMat.cols, srcMat.rows);
+    rectangle(img, rect2, CV_RGB(255, 0, 0), 1, 8, 0);
+    imshow("result",img);
+    waitKey(0);
+    return 0;
+}
+
