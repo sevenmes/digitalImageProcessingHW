8e0f9eb86fcf5ced339a1d54fe399048540eee9c
sevenmes <shuziqi0517@icloud.com>
2020-06-05 16:07:23 +0800

    final

---
+++ final/final/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  final
+//
+//  Created by 舒子琪 on 2020/6/5.
+//  Copyright © 2020 舒子琪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
